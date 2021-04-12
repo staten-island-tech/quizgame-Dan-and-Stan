e1914df38c62799ada782a5417e1983ed79fce9f
StanleyRabkin <71650302+StanleyRabkin@users.noreply.github.com>
2021-04-11 20:31:08 -0400

    taseydjy

---
+++ src/js/index.js
@@ -0,0 +1,149 @@
+const question = {
+  questionContent: "Darth Vader says, 'Luke, I am your Father'",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question2 = {
+  questionContent: "Mace Windu has the only purple lightsaber in Star Wars",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question3 = {
+  questionContent: "Rey is a Skywalker",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question4 = {
+  questionContent: "Darth Maul dies in Episode I",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question5 = {
+  questionContent: "Mace Windu has the only purple lightsaber in Star Wars",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question6 = {
+  questionContent: "Emporer Palpatine's first name is Sheev",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: true,
+    },
+    {
+      choiceContent: "False",
+      correct: false,
+    },
+  ],
+};
+
+const question7 = {
+  questionContent: "Anakin is the chosen one",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: true,
+    },
+    {
+      choiceContent: "False",
+      correct: false,
+    },
+  ],
+};
+
+const question8 = {
+  questionContent: "General Grevious is a droid",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question9 = {
+  questionContent: "Anakin reached the rank of master",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: false,
+    },
+    {
+      choiceContent: "False",
+      correct: true,
+    },
+  ],
+};
+
+const question10 = {
+  questionContent: "Han Solo flew the Kessel run in 12 parsecs",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "True",
+      correct: true,
+    },
+    {
+      choiceContent: "False",
+      correct: false,
+    },
+  ],
+};
