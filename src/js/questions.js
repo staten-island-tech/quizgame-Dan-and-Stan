ea751a768a054197e12b8accbed1e01c75ee6689
DanBel23 <daniel23.bel@gmail.com>
2021-04-30 11:37:31 -0400

    :)

---
+++ src/js/questions.js
@@ -0,0 +1,94 @@
+// Array of questions
+const questions = [
+  {
+    question: "Who is the current chess world champion?",
+    answers: {
+      a: "Magnus Carlsen",
+      b: "Hikaru Nakamura",
+      c: "Lady Gaga",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Which two pieces are involved in castling?",
+    answers: {
+      a: "Carl and Robert",
+      b: "Queen and Rook",
+      c: "King and Rook",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "How many squares are on a chess board?",
+    answers: {
+      a: "2084",
+      b: "64",
+      c: "21",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is the winning move called?",
+    answers: {
+      a: "Checkmate",
+      b: "Checkmark",
+      c: "Shazam",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What FIDE rating is required for the title Fide Master(FM)",
+    answers: {
+      a: "2200",
+      b: "2300",
+      c: "2400",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is the most popular response to 1.e4?",
+    answers: {
+      a: "e5",
+      b: "e6",
+      c: "c5",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is a draw?",
+    answers: {
+      a: "Checkmate",
+      b: "Stalemate",
+      c: "What's up mate",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "How many forms of castling are there?",
+    answers: {
+      a: "Short Castling",
+      b: "Long Castling",
+      c: "Both",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the most popular first move?",
+    answers: {
+      a: "e4",
+      b: "d4",
+      c: "c4",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What is a very bad move called in chess?",
+    answers: {
+      a: "Screw Up",
+      b: "Mistake",
+      c: "Blunder",
+    },
+    correctAnswer: "c",
+  },
+];
+export { questions };
